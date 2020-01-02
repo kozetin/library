daebda058ae49f33a270143ebf45e291d5a18a4d
kozetin <kozetin@yandex.ru>
2020-01-02 18:43:29 +0300

    book view and book removal function created

--- src/main/java/com/kozetin/library/controller/BookController.java
+++ src/main/java/com/kozetin/library/controller/BookController.java
@@ -1,7 +1,41 @@
 package com.kozetin.library.controller;
 
+import com.kozetin.library.domain.Book;
+import com.kozetin.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    private void getListModel(Model model) {
+        List<Book> bookList = new ArrayList<>(bookService.getBookList());
+        model.addAttribute(bookList);
+    }
+
+    @GetMapping
+    public String redirect()
+    {
+        return "redirect:/books";
+    }
+
+    @GetMapping("/books")
+    public String getBookList(Model model) {
+        getListModel(model);
+        return "books";
+    }
+
+    @PostMapping("/removeBook")
+    public String removeBook (Model model, @RequestParam("bookId") String bookId) {
+        bookService.removeBook(bookId);
+        return "redirect:/books";
+    }
 }
--- src/main/java/com/kozetin/library/service/BookService.java
+++ src/main/java/com/kozetin/library/service/BookService.java
@@ -1,7 +1,30 @@
 package com.kozetin.library.service;
 
+import com.kozetin.library.domain.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BookService {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Book> getBookList() {
+        return new ArrayList<>(jdbcTemplate.query(
+                "SELECT id, ISN, name, author FROM book",
+                (rs, rowNum) -> new Book(rs.getLong("id"), rs.getLong("ISN"), rs.getString("name"), rs.getString("author"))
+        ));
+    }
+
+    public void removeBook(String bookid) {
+        Long id = Long.parseLong(bookid);
+        jdbcTemplate.update("DELETE FROM book " +
+                "WHERE book.id = ?",id);
+        jdbcTemplate.update("DELETE FROM book_user " +
+                "WHERE book_user.book_id = ?",id);
+    }
 }
