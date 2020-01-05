e2b384b3e806a13e5ccafb05566549a41cf82090
unknown <kozetin@yandex.ru>
2020-01-05 11:13:37 +0300

    getUserByName function added

--- src/main/java/com/kozetin/library/service/UserService.java
+++ src/main/java/com/kozetin/library/service/UserService.java
@@ -1,7 +1,23 @@
 package com.kozetin.library.service;
 
+import com.kozetin.library.domain.Book;
+import com.kozetin.library.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.ArrayList;
 
 @Service
 public class UserService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public User getUserByName (String name) {
+        return jdbcTemplate.query(
+                "SELECT id, login  password FROM usr " +
+                        "WHERE login =?", new Object[] {name},
+                (rs, rowNum) -> new User(rs.getLong("id"), rs.getString("login"), rs.getString("password"), true)).get(0);
+    }
+
 }
